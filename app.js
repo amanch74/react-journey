ea543f7a180c207f778b02ac71ee2af028de60f4
user.name like : "amanchy2002@gmail.com <amanchy2002@gmail.com>
2026-02-27 15:14:38 +0530

    Episode-01

---
+++ app.js
@@ -0,0 +1,23 @@
+//1st way 
+// const root = document.getElementById('root');
+// root.innerHTML = '<h1> Hello world </h1>';
+
+// 2nd way 
+// const heading = document.createElement("h1");
+// heading.innerHTML = "Hello world again";
+
+// const root = document.getElementById("root");
+// root.appendChild(heading);
+
+const parent = React.createElement('div', {id: 'parent'}, React.createElement('div', {id: 'child'}, [React.createElement("h1",{}, "I am heading"), React.createElement("h2",{}, "I am heading 2")]));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+
+
+// const heading = React.createElement("h1",{id : "heading", xyz: "value"}, 'Hello world with react');
+
+// console.log(heading);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
