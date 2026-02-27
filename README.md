eae56a0be392387699f3ed7b7aede091a24b2965
user.name like : "amanchy2002@gmail.com <amanchy2002@gmail.com>
2026-02-27 19:30:42 +0530

    Episode-02

---
+++ README.md
@@ -0,0 +1,20 @@
+# Namastey React 
+
+# parcel
+- Dev Build
+- Local Server
+- HMR => Hot Module Reload
+- File Watching Algorithm - written in c++
+- Caching - faster builds(in parcel-cache)
+- Image optmisation
+- Minification
+- Budling
+- Compressing
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handlings
+- Https
+- Tree Shaking - remove unused code
+- Different Dev and prod bundles
--- app.js
+++ app.js
@@ -9,6 +9,11 @@
 // const root = document.getElementById("root");
 // root.appendChild(heading);
 
+// now when we install react and react-dom using npm, we can import them in our app.js file and use them to create our react code
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement('div', {id: 'parent'}, React.createElement('div', {id: 'child'}, [React.createElement("h1",{}, "I am heading"), React.createElement("h2",{}, "I am heading 2")]));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
