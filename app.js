ebda6c7adb6b4a3359e8fea0986f3cff38085c2a
user.name like : "amanchy2002@gmail.com <amanchy2002@gmail.com>
2026-02-28 13:44:16 +0530

    Episode-03

--- app.js
+++ app.js
@@ -13,11 +13,53 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { jsx } from "react/jsx-runtime";
 
-const parent = React.createElement('div', {id: 'parent'}, React.createElement('div', {id: 'child'}, [React.createElement("h1",{}, "I am heading"), React.createElement("h2",{}, "I am heading 2")]));
+// React.createElement => ReactElement - js object => htmlElement(render)
+
+const parent = React.createElement(
+    'div', {id: 'parent'}, React.createElement(
+        'div', {id: 'child'}, [
+            React.createElement("h1",{}, "I am heading"),
+            React.createElement("h2",{}, "I am heading 2")
+        ]
+    )
+);
+
+
+// jsx - is not HTML inside js, it is a syntax extension which looks like HTML but it is not HTML, it is a syntax extension which allows us to write HTML like code in our js file and it will be converted to react code by babel
+
+// jsx(transpiled before going to js engine) - done by parcel(package) uses->babel(package) to convert jsx code to react code which can be understood by js engine
+
+// transpiling means converting code from one language to another language, in this case from jsx to react code
+
+// jsxElement => Babel transpiles into React.createElement => ReactElement - js object => htmlElement(render)
+const jsxHeading = <h1> Hello from jsx </h1>
+console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(jsxHeading);
+
+const number = 1000;// we can also use js expressions inside jsx by using curly braces {}
+const element = <h1> The number is {number} </h1> // ReactElement
+
+const TitleComponent = () => {
+    
+    return <div>
+        <HeadingComponent />
+        <HeadingComponent></HeadingComponent>
+        {HeadingComponent()}
+        {number + 200}
+        {element}
+        <h1> Hello from TitleComponent </h1>
+    </div>
+}
+
+const HeadingComponent = () => (
+    <h1> Hello from HeadingComponent </h1>
+)
+
+root.render(<TitleComponent />);
 
 
 
